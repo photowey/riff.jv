7f6211f4df2b89c85d7b383872803633104b0455
photowey <photowey@gmail.com>
2025-07-21 00:55:54 +0800

    feat: add support for VirtualThread
    Signed-off-by: photowey <photowey@gmail.com>

---
+++ infras/riff-infras-authentication/riff-infras-authentication-api/src/main/java/io/github/photowey/riff/infras/authentication/api/config/VirtualThreadExecutorConfiguration.java
@@ -0,0 +1,8 @@
+package io.github.photowey.riff.infras.authentication.api.config;/**
+ * {@code VirtualThreadExecutorConfiguration}.
+ *
+ * @author photowey
+ * @version 1.0.0
+ * @since 2025/07/21
+ */
+public class VirtualThreadExecutorConfiguration {}
